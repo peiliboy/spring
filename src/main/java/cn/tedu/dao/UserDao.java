06779b197b0fe4e20106067b672386deca91b06e
pollard <790717054@qq.com>
2018-07-18 13:45:29 +0800

    呵呵
    丫丫

---
+++ src/main/java/cn/tedu/dao/UserDao.java
@@ -0,0 +1,10 @@
+package cn.tedu.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDao {
+    public void insertUser(){
+        System.out.println("插入成功");
+    }
+}
